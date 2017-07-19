9fd98fe4bc1aa6c9f1dc6191e47375d90ed8086a
Tanyi Chen <t-tachen@microsoft.com>
2017-07-19 16:17:19 +0800

    delete program which is merged to example

--- iothub_client_sample_mqtt.h
+++
@@ -1,9 +0,0 @@
-#ifndef IOTHUB_CLIENT_SAMPLE_MQTT_H
-#define IOTHUB_CLIENT_SAMPLE_MQTT_H
-
-void iothubInit(void);
-void iothubSendMessage(const unsigned char *);
-void iothubLoop(void);
-void iothubClose(void);
-
-#endif
